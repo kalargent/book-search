a4f9adef2f4f8446e5e852e7f1d57336679fa9eb
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-09-11 18:08:53 -0400

    navbar displays

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        {/* <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}> */}
+          Google Books App 
+        {/* </Link> */}
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/search"
+          className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+        >
+          Search
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/saved"
+          className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+        >
+          Saved
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Navbar;
