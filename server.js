1ca36181e9403268840c09675b3c1b1726f71835
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-09-11 18:19:10 -0400

    connected to db

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose"); 
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+// Mongo DB Connection 
+var DB_Connect = process.env.MONGODB_URI || "mongodb://localhost/booksApp"; 
+
+mongoose.connect(DB_Connect, { useNewUrlParser: true }, function (err) {
+    if (err) {
+        console.log(err); 
+    }
+    else {
+        console.log("connected to the db"); 
+    }
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
