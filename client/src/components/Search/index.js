2eac2643ac55bb866057e087f7c9f1e2b9a33f37
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-09-13 13:52:55 -0400

    refactored search component to be stateful so that search can be taken in

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,29 +1,51 @@
-import React from "react"; 
+import React, { Component } from "react";
 
-function BookSearch (props){
-    var text = ""; 
+class BookSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
 
-    function handleInput (e) {
-        text = e.target.value
-    }
-    
-    function handleClick(event){
-        event.preventDefault(); 
-        props.onSearch(text)
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.value);
+  }
+
+  render() {
     return (
-        <div className="search">
-           <form>
-                <div className="form-group">
-                    <label for="search">Search</label>
-                    <input type="text" className="form-control" aria-describedby="emailHelp" onChange={handleInput} placeholder="Search for..."/>
-                </div>
-                <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
-            </form>
-        </div>
-    )
+      <div className="search">
+        <form>
+          <div className="form-group">
+            <label for="search">Search</label>
+            <input
+              type="text"
+              className="form-control"
+              aria-describedby="emailHelp"
+              onChange={this.handleChange}
+              placeholder="Search for..."
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default BookSearch; 
+export default BookSearch;
 
-//https://reactjs.org/docs/forms.html
+
+// //https://reactjs.org/docs/forms.html
